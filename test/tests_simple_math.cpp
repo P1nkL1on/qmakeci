bcc838228ea911ddb2af8376acab11b612f0573d
P1nkL1on <lion6149@mail.com>
2022-12-20 13:57:24 +0300

    Added a bad test

--- test/tests_simple_math.cpp
+++ test/tests_simple_math.cpp
@@ -5,12 +5,18 @@ class TestsSimpleMath : public QObject
     Q_OBJECT
 private slots:
     void compare2and2();
+    void testWhichWontCompile();
     void newBadTest();
 };
 
 void TestsSimpleMath::compare2and2()
 {
     QCOMPARE(2, 2);
+}
+
+void TestsSimpleMath::testWhichWontCompile()
+{
+    return QString("a") / QString("b");
 }
 
 void TestsSimpleMath::newBadTest()
